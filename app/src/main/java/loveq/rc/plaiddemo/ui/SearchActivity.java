9cac80a00038a0c37252fd55b84660c5101f326d
LoveqRc <664215432@qq.com>
2017-12-20 09:22:00 +0800

    第一次提交

---
+++ app/src/main/java/loveq/rc/plaiddemo/ui/SearchActivity.java
@@ -0,0 +1,15 @@
+package loveq.rc.plaiddemo.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import loveq.rc.plaiddemo.R;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
